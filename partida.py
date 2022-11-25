9c575d9ed3634b7b6f03a34856cbc20e604dd407
Felipe Martins <felipe_mmmachado@hotmail.com>
2022-11-25 14:54:53 -0300

    Adicionando icon com link do LinkedIn

--- partida.py
+++ partida.py
@@ -133,6 +133,7 @@ st.markdown('---')
 st.markdown('Trabalho desenvolvido no Minicurso FLAI Data Science na Copa do Mundo!')
 st.markdown('Felipe Martins Machado Mendes')
 
+column1, column2 = st.columns(2)
 st.sidebar.markdown("[![Title]('https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg')]('https://www.linkedin.com/in/felipe-martins-dev/')")
 
 
